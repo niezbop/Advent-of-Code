4b087bdf417e474ff7a6c55a33f28ef093c1060b
Paul Niezborala <pauniezbo@gmail.com>
2025-12-03 15:11:24 +0100

    AoC 2025: day 3 advanced
    Changed approach from checking all combination to actually thinking
    about what the challenge implies in order to be able to ever compute the
    answer (and save my computer).

--- 2025/03_Lobby/advanced.rb
+++ 2025/03_Lobby/advanced.rb
@@ -5,24 +5,19 @@ banks = File.readlines(input_file)
 total = 0
 
 def find_highest_value(batteries, digit_count, start_index = 0)
-  exponent = digit_count - 1
+  highest_digit = batteries[start_index..-digit_count].max
+  
+  return highest_digit if digit_count == 1
 
-  max = -1
-  batteries[start_index..-digit_count].each_with_index do |digit, index|
-    digit_value = (10**exponent) * digit
-    sub_value = digit_count == 1 ? 0 :
-      find_highest_value(batteries, digit_count - 1, index+1)
-    value = digit_value + sub_value
-    max = value if max < value
-  end
-
-  return max
+  factor = 10 ** (digit_count - 1)
+  new_index = batteries[start_index..-digit_count].index(highest_digit) + start_index + 1
+  return highest_digit * factor + find_highest_value(batteries, digit_count - 1, new_index)
 end
 
 banks.each do |bank|
   batteries = bank.strip.chars.map(&:to_i)
   
-  bank_max = find_highest_value(batteries, 2)
+  bank_max = find_highest_value(batteries, 12)
 
   total += bank_max
 end
